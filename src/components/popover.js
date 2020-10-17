86f69da66a961613775b45c0757de7b2fa243306
advait22 <deshpandeadvait88@gmail.com>
2020-10-17 12:59:03 +1100

    first code changes

---
+++ src/components/popover.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Popover extends Component {
+  render() {
+    return (
+      <div style={{height:100,width:100,background:'transparent',position:'absolute'}}>
+        open
+      </div>
+    )
+  }
+}
---
+++ src/components/radius.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Dropdown } from 'semantic-ui-react'
+import './radius.css'
+export default class RadiusDropDown extends Component {
+
+    onSelectRadius = (event) => {
+        this.props.onSelectedRadiusValue(event.target.textContent)
+}
+  render() {
+    return (
+      <div style = {{width:'10rem'}}>
+      <Dropdown
+      placeholder='Select Radius'
+      selection
+      options={this.props.data}
+      onChange={this.onSelectRadius}
+
+
+  />
+      </div>
+    )
+  }
+}
